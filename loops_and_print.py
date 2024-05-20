7f335957c0cdc0efab64991f35443fd2a3bf16ce
francastro1 <162723525+francastro1@users.noreply.github.com>
2024-05-20 14:31:58 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,19 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lista):
+    lista_enumerada = []
+    for indice, valor in enumerate(lista):
+        if valor: 
+            lista_enumerada.append(f"{len(lista_enumerada)}. {valor.title()}")
+    return lista_enumerada
+colors = ["Red","Green","","White","Black",""]
+lista_enumerada = enumerate_list(colors)
+print(lista_enumerada)
 
-
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(lista):
+    lista_invertida = []
+    for indice, valor in enumerate(lista):
+        if valor: 
+            lista_invertida.append(f"{len(lista_invertida)}. {valor[::-1]}")
+    return lista_invertida
+colors = ["Red","Green","","White","Black",""]
+lista_invertida = enumerate_backwards(colors)
+print(lista_invertida)
