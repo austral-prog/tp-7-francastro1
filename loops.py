a62dfd662b550f3eb53906778af16a09eb6021c0
francastro1 <162723525+francastro1@users.noreply.github.com>
2024-05-20 14:28:30 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,51 @@
 def index_of_by_index(word, list, index):
+    for indice in range(index, len(list)):
+        if list[indice]==word:
+            return indice
     return -1
+colors=["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"]
+
+print(index_of_by_index("Black", colors, 1))
+print(index_of_by_index("Black", colors, 4))
+print(index_of_by_index("Green", colors, 2))
 
 
 def index_of_empty(list):
+    for indice, valor in enumerate(list):
+        if valor == "":
+            return indice
     return -1
+colors = ["Red","Green","White","Black","Pink","Yellow","Black"]
+print(index_of_empty(colors))
 
 
 def index_of(word, list):
+    for indice, valor in enumerate(list):
+        if valor == word:
+            return indice
     return -1
+lista = ["Red","Green","White","Black","Pink","Yellow","Black"]
+print(index_of("Black", lista))
+print(index_of("Blue", lista))
 
 
 def put(word, list):
+    for indice, valor in enumerate(list):
+        if valor == '':
+            list[indice]=word
+            return indice
     return -1
+lista = ["Red","Green","","","Pink","","Black"]
+print(put("Black", lista))
 
 
-def remove(word, list):
-    return -1
+def remove(word,list):
+    cuenta = 0 
+    for indice in range(len(list)):
+        if list[indice] == word:
+            list[indice] = ""
+            cuenta = cuenta + 1
+    return cuenta
+colors = ["Red","Green","White","Black","Pink","Yellow","Black"]
+print(remove("Black",colors))
+print(remove("Blue",colors))
